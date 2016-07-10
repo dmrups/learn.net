41c610143485acde87c201884dffa972d4435100
dmrups <archilich@gmail.com>
2016-07-10 15:46:22 +0400

    Lock
    Test project, IL code and short description.

---
+++ lock/LockTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockTest
+{
+    static class Program
+    {
+        static volatile object lockObject = new object();
+
+        static void Main(string[] args)
+        {
+            lock (lockObject)
+            {
+                Console.WriteLine("locked");
+            }
+
+            Console.WriteLine("unlocked");
+            Console.ReadLine();
+        }
+    }
+}
